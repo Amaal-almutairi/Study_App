8d564c0b0711b739b4bfec9b7bf88055b360c6ff
Amaal <amaal772@gmail.com>
2021-10-14 21:41:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/study_app/RVAdapter.kt
@@ -0,0 +1,31 @@
+package com.example.study_app
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.study_app.databinding.ItemRowBinding
+
+
+class RVAdapter(private val activity: Activity, private val items: ArrayList<ArrayList<String>>):
+    RecyclerView.Adapter<RVAdapter.ItemViewHolder>() {
+    class ItemViewHolder(val binding: ItemRowBinding): RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            ItemRowBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        holder.binding.apply {
+            tvTitle.text = items[position][0]
+            tvDetails.text = items[position][1]
+            cvCard.setOnClickListener { CustomAlertDialog(activity, items[position][0], items[position][2]) }
+        }
+    }
+
+    override fun getItemCount() = items.size
+}
